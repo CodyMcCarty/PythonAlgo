eb4bfaee588fad1fb4e5a4f271b158948c600218
Cody McCarty <codyjmccarty@gmail.com>
2022-11-11 10:35:25 -0600

    min_max

--- solutions.py
+++ solutions.py
@@ -11,18 +11,18 @@ class Solution(SolutionInterface):
             year += 1
         return year
 
-    def nb_year2(self, p0, percent, aug, p):
-        percent = percent / 100
-        growth = [p0]
-        while growth[-1] < p:
-            growth.append( growth[-1] + growth[-1] * percent + aug)
-        return len(growth) - 1
+    def testing123(self, lines):
+        return [f"{index}: {line}" for index, line in enumerate(lines, start=1)]
+
+    # keeps ^
+
+    def min_max(lst):
+        return [min(list), max(lst)]
 
     def convert_number_to_reversed_array(self, num):
         return [int(x) for x in reversed(str(num))]
 
-    def testing123(self, lines):
-        return [f"{i}: {v}" for i, v in enumerate(lines, start=1)]
+
 
 if __name__ == '__main__':
     print("python solution")
